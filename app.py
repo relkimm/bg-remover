181c50296337bc53f39963785b7f4a4f19c513ad
relkimm <relkimm@grandeclip.com>
2024-05-12 22:25:56 +0900

    feat: video upload 기능 구현

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from video_uploader import upload_video
 
 app = FastAPI()
 
 @app.get("/")
 def index():
   return { "hello": "world" }
+
+@app.post("/video")
+def video(file: UploadFile):
+  upload_video(f"static/video/{file.filename}", file.file)
+  return { "video": "uploaded" }
---
+++ video_uploader.py
@@ -0,0 +1,3 @@
+def upload_video(path: str, video: any):
+  with open(path, "wb") as buffer:
+    buffer.write(video.read())
